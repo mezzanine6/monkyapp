02c5eb19a2cb0744383ba013df55a615153f0387
evan <evanbennis@gmail.com>
2015-05-08 18:32:23 +0100

    added invalid login errors etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   root               'main_pages#home'
   get  'help'    =>  'main_pages#help'
   get  'about'   =>  'main_pages#about'
   get  'contact' =>  'main_pages#contact'
   get  'signup'  =>  'users#new'
+  get  'login'   =>  'sessions#new'
+  post 'login'   =>  'sessions#create'
+ delete 'logout' =>  'sessions#destroy'
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
